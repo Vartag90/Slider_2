5797b2d0a4541d928631d153ea44bd20dfd10241
Vartag90 <maks.enikarp.90@gmail.com>
2022-06-07 17:15:00 +0300

    last

--- js/script.js
+++ js/script.js
@@ -8,37 +8,95 @@ const sliderContainer = parentSlider.querySelector('.slider__container');
 const sliderBody = parentSlider.querySelector('.slider__body');
 const sliderItems = parentSlider.querySelectorAll('.slider__item');
 const itemsCount = sliderItems.length;
-const btnPrev = parentSlider.querySelector('.btn-next');
-const btnNext = parentSlider.querySelector('.btn-prev');
+const btnPrev = parentSlider.querySelector('.btn-prev');
+const btnNext = parentSlider.querySelector('.btn-next');
 const itemWidth = sliderContainer.clientWidth / slidesToShow;
 const movePosition = itemWidth * slidesToScroll;
+let counter = 0;
+let timerId;
 
 sliderItems.forEach(function(elem) {
     elem.style.minWidth = `${itemWidth}px`;
 })
 
-btnNext.addEventListener('click', function() {
+
+
+function next() {
     const itemLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
     position -= itemLeft >= slidesToScroll ? movePosition : itemLeft * itemWidth;
+    counter += slidesToScroll;
     setPosition();
-    checkBtns();
-})
+    checkPosition();
+}
 
-btnPrev.addEventListener('click', function() {
+function prev() {
     const itemLeft = Math.abs(position) / itemWidth;
-
     position += itemLeft >= slidesToScroll ? movePosition : itemLeft * itemWidth;
-
+    counter -= slidesToScroll;
+    if (counter < 0) {
+        counter = 0;
+    }
     setPosition();
-    checkBtns();
-})
+    checkPosition();
+}
+
+
+function slide() {
+    timerId = setInterval(next, 1000);
+}
+
+window.addEventListener('load', slide);
+
+btnNext.addEventListener('click', next);
+btnNext.addEventListener('click', function() {
+    clearInterval(timerId);
+    setTimeout(slide, 5000);
+});
+
+
+btnPrev.addEventListener('click', prev);
+//btnPrev.addEventListener('click', paused);
+
+
+
+
 
 
 const setPosition = () => sliderBody.style.transform = `translateX(${position}px)`;
 
-const checkBtns = () => {
-    btnPrev.disabled = position === 0;
-    btnNext.disabled = position <= -(itemsCount - slidesToShow) * itemWidth;
+const checkPosition = () => {
+    console.log(counter);
+    if (counter < slidesToShow) {
+        if (counter >= itemsCount - slidesToShow + 1) {
+            sliderBody.style.transform = `translateX(0px)`;
+            position = 0;
+            counter = 0;
+            //imediatle move to first slide
+            sliderBody.style.transition = 0 + `s`;
+
+        } else {
+            //slow slide change
+            sliderBody.style.transition = 0.2 + `s`;
+        }
+    } else {
+        if (counter >= itemsCount) {
+            sliderBody.style.transform = `translateX(0px)`;
+            position = 0;
+            counter = 0;
+            //imediatle move to first slide
+            sliderBody.style.transition = 0 + `s`;
+
+        } else {
+            //slow slide change
+            sliderBody.style.transition = 0.2 + `s`;
+        }
+    }
+
+    //btnPrev.disabled = position === 0;
+    //btnNext.disabled = position <= -(itemsCount - slidesToShow) * itemWidth;
 }
 
-checkBtns();
+
+
+
+checkPosition();

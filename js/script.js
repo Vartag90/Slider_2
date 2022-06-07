936a17e4fa25f726e5af46206b0df06446afa02b
Vartag90 <maks.enikarp.90@gmail.com>
2022-06-07 09:56:45 +0300

    last

--- js/script.js
+++ js/script.js
@@ -1,17 +1,44 @@
 'use strict';
 
-let parent = document.querySelectorAll('.swiper');
+const parentSlider = document.querySelector('.slider');
+let position = 0;
+const slidesToShow = 1;
+const slidesToScroll = 1;
+const sliderContainer = parentSlider.querySelector('.slider__container');
+const sliderBody = parentSlider.querySelector('.slider__body');
+const sliderItems = parentSlider.querySelectorAll('.slider__item');
+const itemsCount = sliderItems.length;
+const btnPrev = parentSlider.querySelector('.btn-next');
+const btnNext = parentSlider.querySelector('.btn-prev');
+const itemWidth = sliderContainer.clientWidth / slidesToShow;
+const movePosition = itemWidth * slidesToScroll;
 
-for (const slide of parent) {
-    let slides = slide.querySelectorAll('.swiper-slide');
-    let i = 0;
-    setInterval(function skip() {
-        slides[i].classList.remove('active');
-        if (i < slides.length - 1) {
-            i++;
-        } else {
-            i = 0;
-        }
-        slides[i].classList.add('active');
-    }, 3000)
+sliderItems.forEach(function(elem) {
+    elem.style.minWidth = `${itemWidth}px`;
+})
+
+btnNext.addEventListener('click', function() {
+    const itemLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
+    position -= itemLeft >= slidesToScroll ? movePosition : itemLeft * itemWidth;
+    setPosition();
+    checkBtns();
+})
+
+btnPrev.addEventListener('click', function() {
+    const itemLeft = Math.abs(position) / itemWidth;
+
+    position += itemLeft >= slidesToScroll ? movePosition : itemLeft * itemWidth;
+
+    setPosition();
+    checkBtns();
+})
+
+
+const setPosition = () => sliderBody.style.transform = `translateX(${position}px)`;
+
+const checkBtns = () => {
+    btnPrev.disabled = position === 0;
+    btnNext.disabled = position <= -(itemsCount - slidesToShow) * itemWidth;
 }
+
+checkBtns();
